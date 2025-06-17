6fabce164c17181a6cb4402ae3fbafe5f3ac4b93
BilalMP <peeroo270@gmail.com>
2025-06-17 19:11:27 +0400

    Multiplication and division

---
+++ Basic/10_multdiv.js
@@ -0,0 +1,13 @@
+/*
+Write a JavaScript program to calculate multiplication and division of two numbers (input from the user).
+*/
+
+function MultDiv(a, b) {
+    return ("Multiplication of " + a + " and " + b + " is " + a * b) + ("\nDivision of " + a + " and " + b + " is " + a / b + "\n");
+}
+
+console.log(MultDiv(5, 6))
+console.log(MultDiv(10, 2))
+console.log(MultDiv(5, 5))
+console.log(MultDiv(25, 5))
+console.log(MultDiv(50, 2))
