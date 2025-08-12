07f9e0ac864f036217802fb1d4df4726648d2316
BilalMP <peeroo270@gmail.com>
2025-08-12 21:30:23 +0400

    Power

---
+++ Functions/15_powers.js
@@ -0,0 +1,8 @@
+/*
+Write a JavaScript function to compute the value of bn where n is the exponent and b is the base. Accept b and n from the user and display the results.
+*/
+
+const powers = (b, n) => b ** n
+
+console.log(powers(2, 3));
+console.log(powers(2, 4));
