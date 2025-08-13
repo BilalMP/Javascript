008db782281f4fb228884bb9d929f60341962724
BilalMP <peeroo270@gmail.com>
2025-08-13 18:37:47 +0400

    Bubble sort

---
+++ Functions/24_bubbleSort.js
@@ -0,0 +1,27 @@
+/*
+Write a JavaScript function to apply the Bubble Sort algorithm.
+Note : According to wikipedia "Bubble sort, sometimes referred to as sinking sort, is a simple sorting algorithm that works by repeatedly stepping through the list to be sorted, comparing each pair of adjacent items and swapping them if they are in the wrong order".
+Sample array : [12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]
+Expected output : [3223, 546, 455, 345, 234, 213, 122, 98, 84, 64, 23, 12, 9, 4, 1]
+*/
+
+const bubbleSort=(arr) =>{
+    let swapped; 
+    for (let i = 0; i < arr.length - 1; i++) {
+        swapped = false;
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = true;
+            }
+        }
+        if (!swapped) {
+            break;
+        }
+    }
+    return arr
+}
+
+console.log(bubbleSort([12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]));
