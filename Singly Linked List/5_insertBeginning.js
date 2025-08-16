b01a61e686de118650ccee65b06c9dd39008455e
BilalMP <peeroo270@gmail.com>
2025-08-16 12:33:09 +0400

    Insert beginning

---
+++ Singly Linked List/5_insertBeginning.js
@@ -0,0 +1,69 @@
+/*
+Write a JavaScript program to insert a node at the beginning of a Singly Linked List.
+*/
+
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    add(value) {
+        if (value === undefined || value === null) {
+            return "Value cannot be undefined or null";
+        }
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            return this;
+        }
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
+        return this;
+    }
+
+    display() {
+        if (!this.head) {
+            return "Empty linked list";
+        }
+        let result = "";
+        let current = this.head;
+        while (current) {
+            result += current.value;
+            if (current.next) {
+                result += " -> ";
+            }
+            current = current.next;
+        }
+        return result + " -> null";
+    }
+
+    addBeginning(value) {
+        if (value === undefined || value === null) {
+            return "Value cannot be undefined or null";
+        }
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode;
+        return this;
+    }
+}
+
+const linkedList = new SinglyLinkedList();
+console.log(linkedList.add(1));
+console.log(linkedList.add(2));
+console.log(linkedList.add(3));
+console.log(linkedList.add(4));
+console.log(linkedList.display());
+console.log(linkedList.addBeginning(5));
+console.log(linkedList.display());
