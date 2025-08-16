39c891d0a5c93986cbc93de81382d33e8c50307e
BilalMP <peeroo270@gmail.com>
2025-08-16 12:32:26 +0400

    Count number of nodes

---
+++ Singly Linked List/3_count.js
@@ -0,0 +1,69 @@
+/*
+Write a JavaScript program to create a singly linked list of n nodes and count the number of nodes.
+*/
+
+class Node{
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    add(value) {
+        if (value === undefined || value === null) {
+            return "Value cannot be undefined or null";
+        }
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+            return this;
+        }
+        let current = this.head;
+        while (current.next) {
+            current = current.next;
+        }
+        current.next = newNode;
+        return this;
+    }
+    display() {
+        if (!this.head) {
+            return "Empty linked list";
+        }
+        let current = this.head;
+        let result = ""; 
+        while (current) {
+            result += current.value;
+            if (current.next) {
+                result += " -> ";
+            }
+            current = current.next;
+        }
+        return result + " -> null";
+    }
+
+    countNode() {
+        if (!this.head) {
+            return 0;
+        }
+        let current = this.head;
+        let count = 0; 
+        while (current) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+}
+
+const linkedList = new SinglyLinkedList();
+console.log(linkedList.add(1));
+console.log(linkedList.add(2));
+console.log(linkedList.add(3));
+console.log(linkedList.add(4));
+console.log(linkedList.display());
+console.log(linkedList.countNode());
