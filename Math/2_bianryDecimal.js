6af4261e05b01b215e9beaa594f2be9c3fe46ba9
BilalMP <peeroo270@gmail.com>
2026-01-13 21:57:11 +0400

    Binary to decimal conversion

---
+++ Math/2_bianryDecimal.js
@@ -0,0 +1,18 @@
+/*
+Write a JavaScript function to convert a binary number to a decimal number.
+Test Data :
+console.log(bin_to_dec('110011'));
+console.log(bin_to_dec('100'));
+51
+4
+*/
+
+const binaryDecimal = (str) => {
+    if (!/^[01]+$/.test(binaryString)) {
+        throw new Error("Invalid binary number. Please provide a binary string.");
+    }
+    return parseInt(str, 2);
+}
+
+console.log(binaryDecimal('110011'));
+console.log(binaryDecimal('100'));
